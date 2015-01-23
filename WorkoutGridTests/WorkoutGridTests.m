8dcab328cc829b871ce1ac1785a5f3ca40ef5e18
jcabrera <jennifer@shelamer.com>
2015-01-23 12:38:22 -0500

    Initial Commit

---
+++ WorkoutGridTests/WorkoutGridTests.m
@@ -0,0 +1,40 @@
+//
+//  WorkoutGridTests.m
+//  WorkoutGridTests
+//
+//  Created by Jennifer Cabrera on 1/23/15.
+//  Copyright (c) 2015 Jennifer Cabrera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface WorkoutGridTests : XCTestCase
+
+@end
+
+@implementation WorkoutGridTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
